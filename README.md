9580dc503b08294a7f1441d4412fb380fa36bdc6
Marmiz <claudio.restifo@gmail.com>
2017-01-02 19:16:04 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+## React - Flux starting kit
+#### A simple environment set up for React.
+
+---
+###### This repo contain:
+- React, react-router and flux
+- Browserify bundling
+- jQuery and Bootstrap for styling
+- Gulp build that:
+
+    -- compiles JSX  
+    -- lints JSX and JS via ESLint  
+    -- bundles JS and CSS files  
+    -- migrates the built app to the dist folder
+
+- Build with node.js so that will:
+
+    -- runs a dev webserver  
+    -- opens your browser at the dev URL  
+    -- reloads the browser upon save  
+
+
+---
+###### To start:
+
+ - make sure you have node.js and Gulp installed.
+ - download or clone this repo.
+ - navigate to your repo directory `cd my/path/`.
+ - `npm install` all the packages listed in the `package.json` file.
+ - type `gulp` in your terminal to launch the app.
+ - navigate to your localhost in case it didn't open. [ http://localhost:9005/]( http://localhost:9005/)
+ - ????
+ - profit
+
+---
+Made by following [Cory House's Starter kit](https://github.com/coryhouse/react-flux-starter-kit)
+example on [pluralsight](https://www.pluralsight.com/authors/cory-house).
+
+:blue_heart:
---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+"use strict";
+
+var gulp = require('gulp');
+var connect = require('gulp-connect'); // run a local web server
+var open = require('gulp-open'); //open URL in a new browser
+var browserify = require('browserify'); //bundle js
+var reactify = require('reactify'); //complie JSX in JS
+var source = require('vinyl-source-stream'); //use conventional text stream with gulp
+var concat = require('gulp-concat'); //Concatenates files
+var lint = require('gulp-eslint'); //Lint JS files, including JSX
+
+
+var config = {
+  port: 9005,
+  devBaseUrl: 'http://localhost',
+  paths: {
+    html: './src/*.html',
+    js: './src/**/*.js',
+    css: [
+        'node_modules/bootstrap/dist/css/bootstrap.min.css',
+        'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+    ],
+    dist: './dist',
+    mainJs: './src/main.js'
+
+  }
+}
+
+// './src/*.html' ---> go into the src folder and find any html file
+
+//start a local dev server
+gulp.task('connect', function() {
+  connect.server({
+    root: ['dist'],
+    port: config.port,
+    base: config.devBaseUrl,
+    livereload: true
+  })
+});
+
+// open a given file --> when you run open first run connect
+gulp.task('open', ['connect'], function() {
+  gulp.src('dist/index.html')
+      .pipe(open({ uri: config.devBaseUrl + ':' + config.port + '/'}));
+})
+
+// get any html file, put it in the dest file defined above then reload.
+gulp.task('html', function() {
+  gulp.src(config.paths.html)
+      .pipe(gulp.dest(config.paths.dist))
+      .pipe(connect.reload());
+});
+
+gulp.task('js', function() {
+  browserify(config.paths.mainJs)
+    .transform(reactify)
+    .bundle() //bundle in the same file
+    .on('error', console.error.bind(console)) //spit error on console
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest(config.paths.dist + '/scripts'))
+    .pipe(connect.reload());
+});
+
+gulp.task('css', function() {
+	gulp.src(config.paths.css)
+		.pipe(concat('bundle.css'))
+		.pipe(gulp.dest(config.paths.dist + '/css'));
+});
+
+gulp.task('lint', function() {
+	return gulp.src(config.paths.js)
+		.pipe(lint({configFile: 'eslint.json'})) //new syntax
+		.pipe(lint.format());
+});
+
+// reload the browser everytime a change is made
+gulp.task('watch', function() {
+  gulp.watch(config.paths.html, ['html']);
+  gulp.watch(config.paths.js, ['js', 'lint']);
+});
+
+// default task
+gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
